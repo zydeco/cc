09aef0fa45027a985b4ab85507fa6bf11ccacca4
Jesús A. Álvarez <zydeco@namedfork.net>
2023-06-02 12:33:41 +0200

    colony

---
+++ colony.lua
@@ -0,0 +1,256 @@
+require("ui")
+
+local colony = nil
+local args = {...}
+if #args == 1 then
+    require("colonyStubs/" .. args[1])
+    colony = COLONY_STUB
+else
+    colony = peripheral.find("colonyIntegrator")
+    if colony == nil then
+        print("No colony integrator")
+        return 1
+    end
+end
+
+local screen = term.current()
+local w,h = screen.getSize()
+local ui = UI.new(screen)
+ui.debug=true
+
+local function notImplementedView(name)
+    return UI.label{
+        x=0,y=0,w=30,h=3,
+        bg=colors.white, fg=colors.red,
+        text=name .. " not implemented",
+    }
+end
+
+local function formatTime()
+    return string.format("d%dt%04.1f", os.day(), os.time())
+end
+
+local function digits(n)
+    return 1 + math.floor(math.log(n, 10))
+end
+
+local statusBarWidth = 8 + digits(os.day())
+
+-- colony name
+ui.add(UI.label{
+    x=0,y=0,w=w-statusBarWidth,h=1,text=colony.getColonyName(),
+    bg=colors.black, fg=colors.white
+})
+
+-- date/time
+local statusBar = UI.label{
+    x=w-statusBarWidth, y=0, w=statusBarWidth, h=1, text=formatTime(), align=UI.RIGHT,
+    bg=colors.black, fg=colors.white
+}
+statusBar.onMouseUp = function() ui.stop() end
+ui.add(statusBar)
+
+-- update every 6 game minutes (5 seconds)
+-- TODO: accurately
+local tabBar = nil
+ui.timer(5.0, nil, function()
+    statusBar.text=formatTime()
+    statusBar.redraw()
+    if tabBar and tabBar.currentTab and tabBar.currentTab.onShow then
+        tabBar.currentTab.onShow()
+    end
+end)
+
+-- sizes
+local contentWidth = w -- 26 on pocket computer
+local contentHeight = h-3 -- 17 on pocket computer
+
+local function filter(list, keep)
+    local filtered = {}
+    for i=1,#list do
+        if keep(list[i]) then
+            filtered[#filtered] = list[i]
+        end
+    end
+    return filtered
+end
+
+local function countMatching(list, matcher)
+    local n = 0
+    for i=1,#list do
+        if matcher(list[i]) then
+            n = n + 1
+        end
+    end
+    return n
+end
+
+local function countNilValues(list, key)
+    return countMatching(list, function(x)
+        return x[key] == nil
+    end)
+end
+
+local function countTrueValues(list, key)
+    return countMatching(list, function(x)
+        return x[key]
+    end)
+end
+
+-- overview view
+local function overview(colony)
+    local box = UI.box{
+        x=0,y=0,w=contentWidth,h=contentHeight,bg=colors.white
+    }
+
+    -- sizes
+    local margin = 2
+    local labelX = margin
+    local labelW = 11
+    local valueX = labelX + labelW + 1
+    local valueW = contentWidth - valueX - (2 * margin)
+    local infoW = w - (2*margin)
+    local y = 1
+
+    -- happiness
+    box.add(UI.label{x=labelX, y=y, text="Happiness:"})
+    local happinessValue = UI.label{x=valueX, y=y, w=valueW, text="n/a"}
+    box.add(happinessValue)
+
+    -- population
+    box.add(UI.label{x=labelX, y=y+1, text="Population:"})
+    local populationValue = UI.label{x=valueX, y=y+1, w=valueW, text="n/a"}
+    box.add(populationValue)
+
+    -- unemployed
+    local unemployedValue = UI.label{x=labelX+2, y=y+2, w=infoW, fg=colors.red}
+    box.add(unemployedValue)
+
+    -- homeless
+    local homelessValue = UI.label{x=labelX+2, y=y+3, w=infoW, fg=colors.red}
+    box.add(homelessValue)
+
+    -- visitors
+    box.add(UI.label{x=labelX, y=y+4, text="Visitors:"})
+    local visitorsValue = UI.label{x=valueX, y=y+4, w=valueW, text="n/a"}
+    box.add(visitorsValue)
+
+    -- buildings
+    box.add(UI.label{x=labelX, y=y+5, text="Buildings:"})
+    local buildingsValue = UI.label{x=valueX, y=y+5, w=valueW, text="n/a"}
+    box.add(buildingsValue)
+
+    -- under construction
+    local underConstructionValue = UI.label{x=labelX+2, y=y+6, w=infoW, fg=colors.blue}
+    box.add(underConstructionValue)
+
+    -- under renovation
+    local underRenovationValue = UI.label{x=labelX+2, y=y+7, w=infoW, fg=colors.blue}
+    box.add(underRenovationValue)
+    
+    -- work orders
+    local workOrdersValue = UI.label{x=labelX, y=y+8, w=infoW, fg=colors.green}
+    box.add(workOrdersValue)
+
+    box.onShow = function()
+        -- update values
+        happinessValue.text = string.format("%.2f/10", colony.getHappiness())
+        populationValue.text = string.format("%d/%d", colony.amountOfCitizens(), colony.maxOfCitizens())
+        local citizens = colony.getCitizens()
+        local unemployed = countNilValues(citizens, "work")
+        unemployedValue.text = string.format("%d unemployed", unemployed)
+        unemployedValue.hidden = (unemployed == 0)
+        local homeless = countNilValues(citizens, "home")
+        homelessValue.text = string.format("%d homeless", homeless)
+        homelessValue.hidden = (homeless == 0)
+        if unemployedValue.hidden and not homelessValue.hidden then
+            homelessValue.y = unemployedValue.y
+        else
+            homelessValue.y = unemployedValue.y + 1
+        end
+        visitorsValue.text = string.format("%d", #colony.getVisitors())
+        local buildings = colony.getBuildings()
+        local realBuildings = countMatching(buildings, function(b)
+            -- don't count postbox or stash as separate buildings
+            return b.type ~= "postbox" and b.type ~= "stash"
+        end)
+        buildingsValue.text = string.format("%d/%d", countTrueValues(buildings, "built"), realBuildings)
+
+        -- buildings under construction
+        local underConstruction = countMatching(buildings, function(b)
+            return b.isWorkingOn and not b.built
+        end)
+        underConstructionValue.text = string.format("%d under construction", underConstruction)
+        underConstructionValue.hidden = (underConstruction == 0)
+
+        -- buildings under renovation
+        local underRenovation = countMatching(buildings, function(b)
+            return b.isWorkingOn and b.built
+        end)
+        underRenovationValue.text = string.format("%d under renovation", underRenovation)
+        underRenovationValue.hidden = (underRenovation == 0)
+        if underConstructionValue.hidden then
+            underRenovationValue.y = underConstructionValue.y
+        else
+            underRenovationValue.y = underConstructionValue.y + 1
+        end
+
+        local workOrders = colony.getWorkOrders()
+        workOrdersValue.text = string.format("%d work orders", countMatching(workOrders, function(order)
+            -- do not include mine building
+            return order.type ~= "WorkOrderMiner"
+        end))
+        workOrdersValue.hidden = (#workOrders == 0)
+        box.redraw()
+    end
+    return box
+end
+
+tabBar = UI.tabBar{x=0,y=1,w=ui.base.w,h=ui.base.h-1,bg=colors.black, tabs={
+    {
+        bg=colors.lightGray, fg=colors.black,
+        key="O", name="Overview",
+        content=overview(colony)
+    },
+    {
+        bg=colors.lime, fg=colors.black,
+        key="C", name="Citizens",
+        content=UI.label({
+            x=0,y=0,w=20,h=2,
+            text="This is the citizens",
+        })
+    },
+    {
+        bg=colors.orange, fg=colors.black,
+        key="B", name="Buildings",
+        content=notImplementedView("Buildings")
+    },
+    {
+        bg=colors.pink, fg=colors.black,
+        key="R", name="Requests",
+        content=notImplementedView("Requests")
+    },
+    {
+        bg=colors.purple, fg=colors.black,
+        key="W", name="Work Orders",
+        content=notImplementedView("Work Orders")
+    },
+    {
+        bg=colors.cyan, fg=colors.black,
+        key="E", name="Employment",
+        content=notImplementedView("Employment")
+    },
+    {
+        bg=colors.green, fg=colors.black,
+        key="H", name="Housing",
+        content=notImplementedView("Housing")
+    },
+    {
+        bg=colors.yellow, fg=colors.black,
+        key="M", name="Map",
+        content=notImplementedView("Map")
+    },
+}}
+ui.add(tabBar)
+
+ui.run()
---
+++ colonyStubs/save.lua
@@ -0,0 +1,83 @@
+ci=peripheral.find("colonyIntegrator")
+c=ci.getCitizens()
+v=ci.getVisitors()
+b=ci.getBuildings()
+w=ci.getWorkOrders()
+--r=ci.getResearch()
+r=ci.getRequests()
+
+funs=""
+for k,v in pairs(ci) do
+  funs = funs .. "\n" .. k
+end
+--textutils.pagedPrint(funs)
+
+function serialize(value)
+  local ok,str = pcall(function()
+    return textutils.serialize(value)
+  end)
+  if ok then
+    return str
+  end
+  str = "{\n"
+  for i=1,#value do
+    str = str .. serialize(value[i]) .. ",\n" 
+  end
+  str = str .. "}"
+  return str
+end
+
+function wf(f, name, value)
+  f.writeLine(name .. "=function()")
+  f.writeLine("  return " .. serialize(value))
+  f.writeLine("end,")
+end
+
+function wm(f, name, values)
+  f.writeLine(name .. "=function(id)")
+  f.writeLine("  if id == nil then")
+  f.writeLine("    return nil")
+  for k,v in pairs(values) do
+    f.writeLine("  elseif id == " .. serialize(k) .. " then")
+    f.writeLine("    return " .. serialize(v))
+  end
+  f.writeLine("  end")
+  f.writeLine("end,")
+end
+
+f = fs.open("stub.lua", "w")
+f.writeLine("COLONY_STUB = {")
+wf(f, "getColonyID", ci.getColonyID())
+wf(f, "getColonyName", ci.getColonyName())
+wf(f, "getColonyStyle", ci.getColonyStyle())
+wf(f, "getLocation", ci.getLocation())
+wf(f, "getHappiness", ci.getHappiness())
+wf(f, "isActive", ci.isActive())
+wf(f, "isUnderAttack", ci.isUnderAttack())
+wf(f, "isUnderRaid", ci.isUnderRaid())
+wf(f, "isInColony", ci.isInColony())
+wf(f, "isWithin", true)
+wf(f, "amountOfCitizens", ci.amountOfCitizens())
+wf(f, "maxOfCitizens", ci.maxOfCitizens())
+wf(f, "amountOfGraves", ci.amountOfGraves())
+wf(f, "amountOfConstructionSites", ci.amountOfConstructionSites())
+wf(f, "getCitizens", c)
+wf(f, "getVisitors", v)
+wf(f, "getBuildings", b)
+wf(f, "getWorkOrders", w)
+wf(f, "getRequests", r)
+
+wor={}
+br={}
+for i=1,#w do
+  local wo = w[i]
+  wor[wo.id] = ci.getWorkOrderResources(wo.id)
+  local builder = wo.builder
+  br[builder] = ci.getBuilderResources(builder)
+end
+
+wm(f, "getWorkOrderResources", wor)
+wm(f, "getBuilderResources", br)
+
+f.writeLine("}")
+f.close()
