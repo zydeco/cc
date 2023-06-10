4294e313a4f8d38fbede365d1fa5e46eb05c1f35
Jesús A. Álvarez <zydeco@namedfork.net>
2023-06-10 21:15:47 +0200

    colony/overview: use multiline label

--- colony/overview.lua
+++ colony/overview.lua
@@ -1,3 +1,64 @@
+local function overviewField(name, value)
+    return string.format("%-14s%s", name .. ": ", value)
+end
+
+local function overviewText(colony)
+    -- general information
+    local lines = {
+        overviewField("Happiness", string.format("%.2f/10", colony.getHappiness())),
+        overviewField("Population", string.format("%d/%d", colony.amountOfCitizens(), colony.maxOfCitizens())),
+    }
+
+    -- citizens
+    local citizens = colony.getCitizens()
+    local unemployed = countNilValues(citizens, "work")
+    if unemployed > 0 then
+        table.insert(lines, string.format("  {red}%d unemployed", unemployed))
+    end
+    local homeless = countNilValues(citizens, "home")
+    if homeless > 0 then
+        table.insert(lines, string.format("  {red}%d homeless", homeless))
+    end
+
+    -- visitors
+    table.insert(lines, overviewField("Visitors", #colony.getVisitors()))
+
+    -- buildings
+    local buildings = colony.getBuildings()
+    local numberOfRealBuildings = countMatching(buildings, function(b)
+        -- don't count postbox or stash as separate buildings
+        return b.type ~= "postbox" and b.type ~= "stash"
+    end)
+    local numberOfBuiltBuildings = countTrueValues(buildings, "built")
+    table.insert(lines, overviewField("Buildings", string.format("%d/%d", numberOfBuiltBuildings, numberOfRealBuildings)))
+
+    -- under construction
+    local numberUnderConstruction = countMatching(buildings, function(b)
+        return b.isWorkingOn and not b.built
+    end)
+    if numberUnderConstruction > 0 then
+        table.insert(lines, string.format("  {blue}%d under construction", numberUnderConstruction))
+    end
+
+    -- under renovation
+    local numberUnderRenovation = countMatching(buildings, function(b)
+        return b.isWorkingOn and b.built
+    end)
+    if numberUnderRenovation > 0 then
+        table.insert(lines, string.format("  {blue}%d under renovation", numberUnderRenovation))
+    end
+
+    -- work orders
+    local workOrders = colony.getWorkOrders()
+    local numberOfWorkOrders = countMatching(workOrders, function(order)
+        -- do not include mine building
+        return order.type ~= "WorkOrderMiner"
+    end)
+    table.insert(lines, overviewField("Work Orders", numberOfWorkOrders))
+
+    return table.concat(lines, "\n")
+end
+
 return function(colony, contentWidth, contentHeight)
 
 local box = UI.Box.new{
@@ -6,103 +67,14 @@ local box = UI.Box.new{
 
 -- sizes
 local margin = 2
-local labelX = margin
-local labelW = 11
-local valueX = labelX + labelW + 1
-local valueW = contentWidth - valueX - (2 * margin)
-local infoW = contentWidth - (2*margin)
-local y = 1
-
--- happiness
-box:add(UI.Label.new{x=labelX, y=y, text="Happiness:"})
-local happinessValue = UI.Label.new{x=valueX, y=y, w=valueW, text="n/a"}
-box:add(happinessValue)
-
--- population
-box:add(UI.Label.new{x=labelX, y=y+1, text="Population:"})
-local populationValue = UI.Label.new{x=valueX, y=y+1, w=valueW, text="n/a"}
-box:add(populationValue)
-
--- unemployed
-local unemployedValue = UI.Label.new{x=labelX+2, y=y+2, w=infoW, fg=colors.red}
-box:add(unemployedValue)
-
--- homeless
-local homelessValue = UI.Label.new{x=labelX+2, y=y+3, w=infoW, fg=colors.red}
-box:add(homelessValue)
-
--- visitors
-box:add(UI.Label.new{x=labelX, y=y+4, text="Visitors:"})
-local visitorsValue = UI.Label.new{x=valueX, y=y+4, w=valueW, text="n/a"}
-box:add(visitorsValue)
-
--- buildings
-box:add(UI.Label.new{x=labelX, y=y+5, text="Buildings:"})
-local buildingsValue = UI.Label.new{x=valueX, y=y+5, w=valueW, text="n/a"}
-box:add(buildingsValue)
-
--- under construction
-local underConstructionValue = UI.Label.new{x=labelX+2, y=y+6, w=infoW, fg=colors.blue}
-box:add(underConstructionValue)
-
--- under renovation
-local underRenovationValue = UI.Label.new{x=labelX+2, y=y+7, w=infoW, fg=colors.blue}
-box:add(underRenovationValue)
-
--- work orders
-local workOrdersValue = UI.Label.new{x=labelX, y=y+8, w=infoW, fg=colors.green}
-box:add(workOrdersValue)
+local dataLabel = UI.Label.new{
+    x=margin, y=1, w=contentWidth - (2*margin), h=(contentHeight - 2),
+}
+box:add(dataLabel)
 
 box.onShow = function()
-    -- update values
-    happinessValue.text = string.format("%.2f/10", colony.getHappiness())
-    populationValue.text = string.format("%d/%d", colony.amountOfCitizens(), colony.maxOfCitizens())
-    local citizens = colony.getCitizens()
-    local unemployed = countNilValues(citizens, "work")
-    unemployedValue.text = string.format("%d unemployed", unemployed)
-    unemployedValue.hidden = (unemployed == 0)
-    local homeless = countNilValues(citizens, "home")
-    homelessValue.text = string.format("%d homeless", homeless)
-    homelessValue.hidden = (homeless == 0)
-    if unemployedValue.hidden and not homelessValue.hidden then
-        homelessValue.y = unemployedValue.y
-    else
-        homelessValue.y = unemployedValue.y + 1
-    end
-    visitorsValue.text = string.format("%d", #colony.getVisitors())
-    local buildings = colony.getBuildings()
-    local realBuildings = countMatching(buildings, function(b)
-        -- don't count postbox or stash as separate buildings
-        return b.type ~= "postbox" and b.type ~= "stash"
-    end)
-    buildingsValue.text = string.format("%d/%d", countTrueValues(buildings, "built"), realBuildings)
-
-    -- buildings under construction
-    local underConstruction = countMatching(buildings, function(b)
-        return b.isWorkingOn and not b.built
-    end)
-    underConstructionValue.text = string.format("%d under construction", underConstruction)
-    underConstructionValue.hidden = (underConstruction == 0)
-
-    -- buildings under renovation
-    local underRenovation = countMatching(buildings, function(b)
-        return b.isWorkingOn and b.built
-    end)
-    underRenovationValue.text = string.format("%d under renovation", underRenovation)
-    underRenovationValue.hidden = (underRenovation == 0)
-    if underConstructionValue.hidden then
-        underRenovationValue.y = underConstructionValue.y
-    else
-        underRenovationValue.y = underConstructionValue.y + 1
-    end
-
-    local workOrders = colony.getWorkOrders()
-    workOrdersValue.text = string.format("%d work orders", countMatching(workOrders, function(order)
-        -- do not include mine building
-        return order.type ~= "WorkOrderMiner"
-    end))
-    workOrdersValue.hidden = (#workOrders == 0)
-    box:redraw()
+    dataLabel.text = overviewText(colony)
+    dataLabel:redraw()
 end
 
 box.refresh = box.onShow
